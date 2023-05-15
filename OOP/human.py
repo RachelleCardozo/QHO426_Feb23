cf0c7898b5664c0ad1169ea297d37483ae404582
Rachelle Cardozo <rachellecardozo95@gmail.com>
2023-05-15 11:27:01 +0100

    week 11 inharitance lecture

--- OOP/human.py
+++ OOP/human.py
@@ -1,10 +1,10 @@
-class Human:
+from inhabitant import Inhabitant
+
+class Human(Inhabitant):
     MAX_ENERGY = 100
 
-    def __init__(self):
-        self.name = "Human"
-        self.age = 0
-        self.energy = Human.MAX_ENERGY
+    def __init__(self, name ="Human", age=0, energy=MAX_ENERGY):
+        super().__init__(name, age, energy)
 
     def __str__(self):
         return f"{self.name} of age {self.age} has {self.energy} energy."
@@ -12,21 +12,7 @@ class Human:
     def __repr__(self):
         return f"Human(name={self.name}, age= {self.age}, energy={self.energy})"
 
-    def display(self):
-        print(f"I am {self.name}.")
 
-    def grow(self):
-        self.age += 1
-
-    def eat(self,amount):
-        self.energy += amount
-        if self.energy > Human.MAX_ENERGY:
-            self.energy = Human.MAX_ENERGY
-
-    def move(self, distance):
-        self.energy -= distance
-        if self.energy < 0:
-            self.energy = 0
 
 
 
--- OOP/robot.py
+++ OOP/robot.py
@@ -1,10 +1,10 @@
-class Robot:
+from inhabitant import Inhabitant
+
+class Robot(Inhabitant):
     MAX_ENERGY = 100
 
-    def __init__(self):
-        self.name = "Robot"
-        self.age = 0
-        self.energy = 0
+    def __init__(self, name="Robot", age=0, energy=0):
+        super().__init__(name, age, energy)
 
     def __str__(self):
         return f"{self.name} of age {self.age} has {self.energy} energy."
@@ -12,25 +12,11 @@ class Robot:
     def __repr__(self):
         return f"Robot(name={self.name}, age= {self.age}, energy={self.energy})"
 
-    def display(self):
-        print(f"I am {self.name}.")
 
-    def grow(self):
-        self.age += 1
-
-    def eat(self,amount):
-        self.energy += amount
-        if self.energy > Robot.MAX_ENERGY:
-            self.energy = Robot.MAX_ENERGY
-
-    def move(self, distance):
-        self.energy -= distance
-        if self.energy < 0:
-            self.energy = 0
 
 
 if __name__ == "__main__":
-    r = Robot()
+    r = Robot("Walle",12, 88 )
     r.display()
     print(r)
     print(repr(r))
